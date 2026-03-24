961c6cf529f47cf689bac074f7d3544442c927ef
Nicolasfreitas6 <nicolasliceski@hotmail.com>
2026-03-24 01:00:32 -0300

    Time: 908 ms (74.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,11 @@
+#--Tipo: JOIN + agregação
+#--Conceito: CROSS JOIN + LEFT JOIN + GROUP BY + COUNT
+#--Problema: Students and Examinations (count exams per student per subject)
+SELECT s1.student_id, s1.student_name, s2.subject_name, COUNT(e.subject_name) AS attended_exams
+FROM students s1
+CROSS JOIN subjects s2
+LEFT JOIN examinations e
+    ON s1.student_id = e.student_id
+    AND s2.subject_name = e.subject_name
+GROUP BY s1.student_id, s1.student_name, s2.subject_name
+ORDER BY s1.student_id, s2.subject_name
