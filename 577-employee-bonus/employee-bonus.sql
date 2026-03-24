747156e203e8043d20ecdb574c67342d4c47937b
Nicolasfreitas6 <nicolasliceski@hotmail.com>
2026-03-24 00:51:45 -0300

    Time: 944 ms (51.39%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
+#--Tipo: JOIN + filtro com NULL
+#--Conceito: LEFT JOIN + WHERE + OR + IS NULL
+#--Problema: Employee Bonus (bonus < 1000 ou sem bonus)
 SELECT e.name, b.bonus
 FROM employee e
 LEFT JOIN bonus b
